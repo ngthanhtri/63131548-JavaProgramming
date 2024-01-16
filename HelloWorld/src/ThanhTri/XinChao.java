0b1e9308e16995af7eec5fedb0ec555490fb9070
thanhtri2112 <thanhtri21120309@gmail.com>
2024-01-16 21:08:59 +0700

    Update XinChao.java

--- HelloWorld/src/ThanhTri/XinChao.java
+++ HelloWorld/src/ThanhTri/XinChao.java
@@ -2,17 +2,9 @@
  * 
  */
 package ThanhTri;
-
-/**
- * 
- */
 public class XinChao {
-
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("TÔI LẦN ĐẦU HỌC JAVA, và đang thử nghiệm quản lý mã nguồn với GIT");
 
 	}
 
