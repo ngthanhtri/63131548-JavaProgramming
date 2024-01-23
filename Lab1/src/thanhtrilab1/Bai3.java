13eb52acd88db56c352054a1cac495cd68a2909e
thanhtri2112 <thanhtri21120309@gmail.com>
2024-01-23 18:29:02 +0700

    Code bài 3 lab 1

---
+++ Lab1/src/thanhtrilab1/Bai3.java
@@ -0,0 +1,11 @@
+package thanhtrilab1;
+import java.util.Scanner;
+public class Bai3 {
+	public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhập cạnh của khối lập phương: ");
+        double canh = scanner.nextDouble();
+        double theTich = Math.pow(canh, 3);
+        System.out.printf("Thể tích của khối lập phương: %.1f\n", theTich);
+    }
+}
