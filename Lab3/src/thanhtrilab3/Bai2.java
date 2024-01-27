af8f7ecf026425af9f1c6ee8eadb2bd42544bb0e
thanhtri2112 <thanhtri21120309@gmail.com>
2024-01-27 17:21:37 +0700

    Code bài 2 lab 3

---
+++ Lab3/src/thanhtrilab3/Bai2.java
@@ -0,0 +1,13 @@
+package thanhtrilab3;
+
+public class Bai2 {
+	public static void main(String[] args) {
+        for (int i = 1; i <= 9; i++) {
+            System.out.printf("Bảng cửu chương %d:\n", i);
+            for (int j = 1; j <= 10; j++) {
+                System.out.printf("%d x %d = %d\n", i, j, i * j);
+            }
+            System.out.println();
+        }
+    }
+}
