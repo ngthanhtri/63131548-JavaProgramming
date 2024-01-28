77385a32e372e9aea4482c4cc12b7d4949de6cb1
thanhtri2112 <thanhtri21120309@gmail.com>
2024-01-28 16:09:47 +0700

    Code bài 2 lab 4

---
+++ Lab4/src/thanhtrilab4/Bai2.java
@@ -0,0 +1,48 @@
+package thanhtrilab4;
+import java.util.Scanner;
+
+public class Bai2 {
+    public static void main(String[] args) {
+        SanPham sp1 = new SanPham();
+        SanPham sp2 = new SanPham();
+
+        System.out.println("Nhập thông tin sản phẩm 1:");
+        sp1.nhap();
+
+        System.out.println("Nhập thông tin sản phẩm 2:");
+        sp2.nhap();
+
+        System.out.println("Thông tin sản phẩm 1:");
+        sp1.xuat();
+
+        System.out.println("Thông tin sản phẩm 2:");
+        sp2.xuat();
+    }
+
+    public static class SanPham {
+        private String tenSp;
+        private double donGia;
+        private double giamGia;
+
+        public double getThueNhapKhau() {
+            return 0.1 * donGia;
+        }
+
+        public void xuat() {
+            System.out.println("Tên sản phẩm: " + tenSp);
+            System.out.println("Đơn giá: " + donGia);
+            System.out.println("Giảm giá: " + giamGia);
+            System.out.println("Thuế nhập khẩu: " + getThueNhapKhau());
+        }
+
+        public void nhap() {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Nhập tên sản phẩm: ");
+            tenSp = scanner.nextLine();
+            System.out.print("Nhập đơn giá: ");
+            donGia = scanner.nextDouble();
+            System.out.print("Nhập giảm giá: ");
+            giamGia = scanner.nextDouble();
+        }
+    }
+}
