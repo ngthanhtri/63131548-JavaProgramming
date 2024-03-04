aee7ce606bae13267decfd526f7c5ea94edda0f3
thanhtri2112 <thanhtri21120309@gmail.com>
2024-03-04 19:24:11 +0700

    Code TH_Bai*

---
+++ TH_Bai0/src/thanhtribai0/ChuongTrinhTinhToan.java
@@ -0,0 +1,10 @@
+package thanhtribai0;
+
+public class ChuongTrinhTinhToan {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new ManHinhTinhToan().setVisible(true);
+	}
+
+}
---
+++ TH_Bai1/src/thanhtribai1/CTC.java
@@ -0,0 +1,10 @@
+package thanhtribai1;
+
+public class CTC {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new LoginFrame().setVisible(true);
+	}
+
+}
